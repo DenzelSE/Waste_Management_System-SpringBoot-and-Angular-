29f06828183f947de80d24c122b34481e3801247
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-06-27 23:58:48 +0200

    connecting spring and html

--- waste-management-frontend/src/app/services/waste.service.ts
+++ waste-management-frontend/src/app/services/waste.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WasteService {
+  private baseUrl = 'http://localhost:8080/api'; // change this if different
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllWasteItems(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/waste-items`);
+  }
+
+  getAllCategories(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/categories`);
+  }
+
+  addWasteItem(item: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/waste-items`, item);
+  }
 }
