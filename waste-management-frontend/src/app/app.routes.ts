340af743dfe26c90fc15ab22a3d63706923bd401
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-06-27 23:57:03 +0200

    app routing config

--- waste-management-frontend/src/app/app.routes.ts
+++ waste-management-frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WasteItemsComponent } from './components/waste-items/waste-items.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'waste-items', component: WasteItemsComponent },
+  { path: 'categories', component: CategoriesComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
