d686ede6be6aa47e3807fe2fe031eb366c79ddca
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-06-29 18:36:50 +0200

    services

--- waste-management-backend/src/main/java/denzelse/waste_management/Controller/DisposalGuidelineController.java
+++ waste-management-backend/src/main/java/denzelse/waste_management/Controller/DisposalGuidelineController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import denzelse.waste_management.DTOs.DisposalGuidelineDTO;
+import denzelse.waste_management.Service.DisposalGuidelineServiceImp;
+
 
 /**
  * Controller for managing diposal guidelines in the Waste Management Spring Boot application.
